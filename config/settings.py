71fc761f3384e32e7ae9c39c4d3c2792c64e9332
Anastasia Chernysheva <nastya770098@gmail.com>
2024-04-16 16:15:01 +0300

    docker

--- config/settings.py
+++ config/settings.py
@@ -80,17 +80,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ed_modules',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'nastasia1',
-        'HOST': 'db',
-        'PORT': '5432'
+        'PASSWORD': 'mysecretpassword',
+        'HOST': 'db'
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
